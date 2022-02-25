5f44aa81a05a108bdbf47ea68cccea53fd66af6f
Michael Law <mikelaw86@hotmail.com>
2022-02-25 10:37:40 -0500

    Overwriting odd checker

--- odd.js
+++ odd.js
@@ -1,6 +1,10 @@
-const isOdd  = function (num) {
-  return num % 3 === 0;
+const isOdd = function(num){
+  if(num % 2 === 0){
+    return false;
+  }
+  return true;
 }
 
 console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
+console.log("45 is odd: " + isOdd(45));
