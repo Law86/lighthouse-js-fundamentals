aa1af43871c9e290f65b38fcbaf075b730189c74
Michael Law <mikelaw86@hotmail.com>
2022-02-25 19:32:30 -0500

    List for packing assignment

---
+++ packing-list.js
@@ -0,0 +1,9 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];  
+
+
+const moving = packingList =>{
+console.log("Kitchen stuff to pack:")
+  for (var i = 0 ; i < packingList.length ; i++){
+  console.log(packingList[i])
+  }
+}
